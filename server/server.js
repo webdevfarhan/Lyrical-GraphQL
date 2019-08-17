47ea5e860936e4c4cca7ffc0229a17b8bbbd03bd
Farhan <frhnmdni@gmail.com>
2019-08-17 17:00:41 +0530

    add mongodb uri

--- server/server.js
+++ server/server.js
@@ -8,22 +8,23 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://admin:admin@cluster0-htqmo.gcp.mongodb.net/test?retryWrites=true&w=majority';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
-mongoose.connection
-    .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error));
+mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+mongoose.connection.once('open', () => console.log('Connected to MongoLab instance.')).on('error', error => console.log('Error connecting to MongoLab:', error));
 
 app.use(bodyParser.json());
-app.use('/graphql', expressGraphQL({
-  schema,
-  graphiql: true
-}));
+app.use(
+  '/graphql',
+  expressGraphQL({
+    schema,
+    graphiql: true
+  })
+);
 
 const webpackMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
