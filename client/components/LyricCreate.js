31ef533367de353b4cc7162c1126f708b37221d1
Farhan <frhnmdni@gmail.com>
2019-08-19 20:09:21 +0530

    add thumbs-up icon

--- client/components/LyricCreate.js
+++ client/components/LyricCreate.js
@@ -37,6 +37,7 @@ const mutation = gql`
     addLyricToSong(content: $content, songId: $songId) {
       id
       lyrics {
+        id
         content
       }
     }
--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
 class LyricsList extends Component {
+  onLike(id) {
+    console.log(id);
+  }
+
   renderLyrics() {
     return this.props.lyrics.map(({ id, content }) => {
       return (
         <li className='collection-item' key={id}>
           {content}
+          <i className='material-icons' onClick={() => this.onLike(id)}>
+            thumb_up
+          </i>
         </li>
       );
     });
